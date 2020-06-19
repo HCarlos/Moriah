c8f90328dc0b587916e224e013b71a7c23ce44b1
HCarlos <r0@tecnointel.mx>
2020-06-19 15:26:22 +0000

    Moriah - Ej_i | Alfa L5.8

--- app/Http/Controllers/Externos/PaqueteExternoController.php
+++ app/Http/Controllers/Externos/PaqueteExternoController.php
@@ -89,7 +89,7 @@ class PaqueteExternoController extends Controller{
         $Referencia    = $arr[8];
         $Observaciones = $arr[9];
         $CadenaUsuario = $arr[10];
-        $TotalInternet = floatval($arr[10]);
+        $TotalInternet = floatval($arr[11]);
         return Response::json([
             'mensaje' => 'OK', 
             'data' => $arr[0].' - '.$arr[1].' - '.$arr[2].' - '.$arr[3].' - '.$arr[4].' - '.$arr[5].' - '.$arr[6].' - '.$arr[7].' - '.$arr[8].' - '.$arr[9].' - '.$arr[10].' - '.$TotalInternet, 
