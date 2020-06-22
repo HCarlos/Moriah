621a9c0df3f571b37a39d4d7aaae198b423e148d
HCarlos <r0@tecnointel.mx>
2020-06-22 15:58:58 +0000

    Moriah - Ek_c | Alfa L5.8

--- app/Http/Controllers/Externos/PedidoPlatsourceController.php
+++ app/Http/Controllers/Externos/PedidoPlatsourceController.php
@@ -110,13 +110,13 @@ class PedidoPlatsourceController extends Controller{
         $pdf->setX(10);
 
         $pdf->SetFont('Arial','',8);
-        $pdf->Cell(60,$this->alto,"REFERENCIA Ó CONCEPTO PARA PAGAR: ",0,0,"L");
+        $pdf->Cell(60,$this->alto,utf8_decode("REFERENCIA Ó CONCEPTO PARA PAGAR: "),0,0,"L");
         $pdf->SetFont('Arial','B',10);
         $pdf->Cell(50,$this->alto,$Ped->referencia,0,1,"L");
         $pdf->setX(10);
 
         $pdf->SetFont('Arial','',10);
-        $pdf->Cell(195,$this->alto,$Ped->observaciones,0,1,"L");
+        $pdf->Cell(195,$this->alto,utf8_decode($Ped->observaciones),0,1,"L");
         $pdf->setX(10);
 
         $pdf->Ln();
