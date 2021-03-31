ff41635ea5d430fa5434cec550d96227487e76fa
HCarlos <r0@tecnointel.mx>
2021-03-31 01:13:29 +0000

    Moriah - N0019 | Alfa L7.28

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use App\Providers\Debugbar;
 use Barryvdh\Debugbar\Middleware\DebugbarEnabled;
-use DebugBar\DebugBar as DebugBarDebugBar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Blade::withoutComponentTags();
+
         Blade::component('catalogos.share._panel_list','panel');
     }
 
