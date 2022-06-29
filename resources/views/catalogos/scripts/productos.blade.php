34d5a89a2e5f2a12a52f8206eddfbfb72b4f5a4e
HCarlos <r0@tecnointel.mx>
2022-06-29 14:39:18 +0000

    Moriah - V2_A18a | Alfa L7.30.4

--- resources/views/catalogos/scripts/productos.blade.php
+++ resources/views/catalogos/scripts/productos.blade.php
@@ -31,7 +31,7 @@
                     $("#myModal .modal-body").html('<div class="fa-2x"><i class="fa fa-cog fa-spin"></i> Actualizando producto...</div>');
                     $("#myModal").modal('show');
                     var Url = event.currentTarget.id;
-                    alert(Url);
+                    // alert(Url);
                     $(function () {
                         $.ajax({
                             method: "get",
